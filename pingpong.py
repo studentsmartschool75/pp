70beba0125e060197d0692d1da219812f691dc81
studentsmartschool75 <116977780+studentsmartschool75@users.noreply.github.com>
2023-05-30 07:39:36 +0300

    Update pingpong.py
    v4

--- pingpong.py
+++ pingpong.py
@@ -55,6 +55,9 @@ racket2 = Player('racket.png', 520, 200, 50, 150, 4)
 ball = GameSprite('tenis_ball.png', 200, 200, 50, 50, 4)
 
 
+#движение мяча
+dx = 3
+dy = 3
 
 
 game = True
@@ -68,6 +71,8 @@ while game:
     if finish == False:
         racket1.update_l()
         racket2.update_r()
+        ball.rect.x += dx
+        ball.rect.y += dy
        
         window.fill((100,100,100))
         racket1.reset()
